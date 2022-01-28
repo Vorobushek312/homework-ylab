79b86e8d4c53d11a135520f285f950759f694d1d
Vorobushek312 <Voronov_95@list.ru>
2022-01-28 16:26:52 +0600

    new_version

--- HomeWorkYlab1.py
+++ HomeWorkYlab1.py
@@ -1,25 +1,39 @@
 # Домашнее задание урок №1
 # Воронов Андрей
+from cmath import inf
+import itertools
+
+
 def range_a_to_b(a,b):
     return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
 
 
-def all_range(post, point_1, point_2, point_3, point_4):
-    step_1 = range_a_to_b(post, point_1)
-    step_2 = range_a_to_b(point_1, point_2)
-    two_step = step_1 + step_2
-    step_3 = range_a_to_b(point_2, point_3)
-    three_step = two_step + step_3
-    step_4 = range_a_to_b(point_3, point_4)
-    four_step = three_step + step_4
-    step_5 = range_a_to_b(point_4, post)
-    five_step = four_step + step_5
-    text = f'{str(post)} -> {str(point_1)}[{step_1}] \
--> {str(point_2)}[{two_step}] -> {str(point_3)}[{three_step}] -> {str(point_4)}[{four_step}] \
--> {str(post)}[{five_step}] = {five_step}'
-    return  five_step, text
+def all_range_updata(*args):
+    index_value = 0
+    a = str()
+    text = str()
+    answer = 0
+    len_args = len(args)
+    for arg in args:
+        if index_value == 0:
+            a = arg
+            a_last = arg
+            text += str(a)
+        else:
+            step = range_a_to_b(a, arg)
+            answer += step
+            text += f'{arg}[{answer}]'
+            a = arg
+        text += ' -> '
+        index_value += 1
+        if index_value == len_args:
+            step = range_a_to_b(a, a_last)
+            answer += step
+            text += f'{arg}[{answer}] = {answer}'
+            return answer, text
 
-min = 10000
+
+min_value = float(inf)
 options = []
 index = 0
 a = (0, 2)
@@ -27,37 +41,21 @@ b = (2, 5)
 c = (5, 2)
 d = (6, 6)
 e = (8, 3)
-roads = [(a, b, c, d, e),
-        (a, b, c, e, d),
-        (a, b, d, c, e),
-        (a, b, d, e, c),
-        (a, b, e, c, d),
-        (a, b, e, d, c),
-        (a, c, b, e, d),
-        (a, c, b, d, e),
-        (a, c, d, e, b),
-        (a, c, d, b, e),
-        (a, c, e, b, d),
-        (a, c, e, d, b),
-        (a, e, b, c, d),
-        (a, e, b, d, c),
-        (a, e, c, b, d),
-        (a, e, c, d, b),
-        (a, e, d, b, c),
-        (a, e, d, c, b),
-        (a, d, e, b, c),
-        (a, d, e, c, b),
-        (a, d, b, e, c),
-        (a, d, b, c, e),
-        (a, d, c, e, b),
-        (a, d, c, b, e),]
-for a, b, c, d, e in roads: # Первый раз вызываю функцию необходимо найти наименьшую длину пути
-    ansver = all_range(a, b, c, d, e)
-    if ansver[0] <= min:
-        min = ansver[0]
-for a, b, c, d, e in roads: # Второй раз вызываю функцию ищим совподаюшие длины путей
-    ansver = all_range(a, b, c, d, e)
-    if ansver[0] == min:
+roads = []
+roads_gener = itertools.permutations([b,c,d,e], 4)
+for road in roads_gener:
+    road_list = []
+    road_list.append(a) 
+    for point in road:
+        road_list.append(point) 
+    roads.append(road_list)
+for road in roads:
+    answer = all_range_updata(road[0], road[1], road[2], road[3], road[4])
+    if answer[0] <= min_value:
+        min_value = answer[0]
+for road in roads:
+    answer = all_range_updata(road[0], road[1], road[2], road[3], road[4])
+    if answer[0] == min_value:
         index += 1
         print('Вариант: ' + str(index))
-        print(ansver[1])
+        print(answer[1])
