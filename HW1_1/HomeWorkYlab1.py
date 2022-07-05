80c4e2feae22560d38af2caffa628440830d0b4e
Vorobushek312 <Voronov_95@list.ru>
2022-07-05 16:07:01 +0600

    Update HomeWorkYlab1.py

--- HW1_1/HomeWorkYlab1.py
+++ HW1_1/HomeWorkYlab1.py
@@ -8,13 +8,12 @@ def range_a_to_b(a,b):
     return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
 
 
-def all_range_updata(*args):
+def all_range_updata(list_place):
     index_value = 0
     a = str()
     text = str()
     answer = 0
-    len_args = len(args)
-    for arg in args:
+    for arg in list_place:
         if index_value == 0:
             a = arg
             a_last = arg
@@ -26,7 +25,7 @@ def all_range_updata(*args):
             a = arg
         text += ' -> '
         index_value += 1
-        if index_value == len_args:
+        if index_value == len(list_place):
             step = range_a_to_b(a, a_last)
             answer += step
             text += f'{a_last}[{answer}] = {answer}'
@@ -36,26 +35,23 @@ def all_range_updata(*args):
 min_value = float(inf)
 options = []
 index = 0
-a = (0, 2)
-b = (2, 5)
-c = (5, 2)
-d = (6, 6)
-e = (8, 3)
+list_place = [(0, 2), (2, 5), (5, 2), (6, 6), (8, 3)]
 roads = []
-roads_gener = itertools.permutations([b,c,d,e], 4)
+roads_gener = itertools.permutations(list_place[1:], len(list_place) - 1)
 for road in roads_gener:
     road_list = []
-    road_list.append(a) 
+    road_list.append(list_place[0]) 
     for point in road:
         road_list.append(point) 
     roads.append(road_list)
 for road in roads:
-    answer = all_range_updata(road[0], road[1], road[2], road[3], road[4])
+    answer = all_range_updata(road)
     if answer[0] <= min_value:
         min_value = answer[0]
 for road in roads:
-    answer = all_range_updata(road[0], road[1], road[2], road[3], road[4])
+    answer = all_range_updata(road)
     if answer[0] == min_value:
         index += 1
         print('Вариант: ' + str(index))
         print(answer[1])
+
