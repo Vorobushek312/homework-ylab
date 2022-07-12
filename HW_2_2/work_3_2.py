df9ae9b452adb4aba09f57c612a72ebeb102ea8e
Vorobushek312 <Voronov_95@list.ru>
2022-07-12 23:48:01 +0600

    Update work_3_2.py

--- HW_2_2/work_3_2.py
+++ HW_2_2/work_3_2.py
@@ -1,26 +1,29 @@
 import time
-def decorator_arg(func):
-    def wrapper(call_count, start_sleep_time, factor, border_sleep_time):
-        count = 1
-        print(f"Кол-во запусков = call_count ({call_count})\nНачало работы")
-        while count <= call_count:
-            status = func
-            if status == True:
-                break
-            print(f"Запуск номер {count}. Ожидание: {start_sleep_time} секунд. Результат декорируемой функций = {status()}")
-            time.sleep(start_sleep_time)
-            count += 1
-            if start_sleep_time < border_sleep_time:
-                start_sleep_time *= start_sleep_time*2**factor
-            if start_sleep_time >= border_sleep_time:
-                start_sleep_time = border_sleep_time
-        print("Конец работы")
-    return wrapper
+def decorator_maker_with_arguments(call_count, start_sleep_time, factor, border_sleep_time ):
+    def decorator_arg(func):
+        def wrapper():
+            t = start_sleep_time
+            count = 1
+            print(f"Кол-во запусков = call_count ({call_count})\nНачало работы")
+            while count <= call_count:
+                status = func
+                if status == True:
+                    break
+                print(f"Запуск номер {count}. Ожидание: {t} секунд. Результат декорируемой функций = {status()}")
+                time.sleep(t)
+                count += 1
+                if t < border_sleep_time:
+                    t *= t*2**factor
+                if t >= border_sleep_time:
+                    t = border_sleep_time
+            print("Конец работы")
+        return wrapper
+    return decorator_arg
 
 
-@decorator_arg
-def func(call_count = 3, start_sleep_time = 1, factor = 3, border_sleep_time = 15):
+@decorator_maker_with_arguments(3, 1, 3, 15)
+def func():
     return False
 
 if __name__ == '__main__':
-    func(call_count = 3, start_sleep_time = 1, factor = 2, border_sleep_time = 20)
+    func()
